20bdb59893abc0e43368222840ecc18ac634d0b9
Christian Toivola <c.toivola@gmail.com>
2013-05-02 15:23:41 -0400

    Adding a test for broken ampersand parsing

--- fooxml/tests/xml_open.py
+++ fooxml/tests/xml_open.py
@@ -1,3 +1,4 @@
+from StringIO import StringIO
 import unittest
 
 import fooxml
@@ -29,4 +30,44 @@ class XmlOpenTest(unittest.TestCase):
         assert objs[0]['SortOrder'] == '2'
         assert objs[0]['p_BaseObjectID'] == 'MN0000833164'
         assert objs[0]['Action'] == 'A'
+
+
+    def test_parse_ampersand(self):
+        """
+        Test parsing of ampersand characters
+        """
+        # From http://www.w3schools.com/xml/simple.xml
+        xml_fragment = """<?xml version="1.0" encoding="ISO-8859-1"?>
+        <!-- Edited by XMLSpy? -->
+        <breakfast_menu>
+            <food>
+                <name>Belgian Waffles &amp; Beer</name>
+                <price>$5.95</price>
+                <description>two of our famous Belgian Waffles with plenty of real maple syrup</description>
+                <calories>650</calories>
+            </food>
+            <food>
+                <name>Strawberry Belgian Waffles</name>
+                <price>$7.95</price>
+                <description>light Belgian waffles covered with strawberries and whipped cream</description>
+                <calories>900</calories>
+            </food>
+        </breakfast_menu>
+        """
+        stream = StringIO(xml_fragment)
+        stream.seek(0)
+
+        objs = []
+
+        def callback(obj):
+            objs.append(obj)
+
+        handler = fooxml.SimpleHandler("food", callback=callback)
+        xml_file = fooxml.xml_file(stream, handler)
+        xml_file.parse()
+
+        assert len(objs) > 1
+        assert type(objs[0]) == dict
+        assert objs[0]['name'] == 'Belgian Waffles & Beer'
+
 
